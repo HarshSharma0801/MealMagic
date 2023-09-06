bfc21444aaf0892a5540bd01b69940e1eeba5315
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-09-06 20:00:11 +0530

    update

--- MealMagic/src/App.jsx
+++ MealMagic/src/App.jsx
@@ -3,42 +3,35 @@ import Home from "./Pages/Home";
 import ItemsList from "./Components/Items/ItemsList";
 import { Routes, Route } from "react-router-dom";
 import ItemsContext from "./ContextAPI/ItemsContext";
-import { useState,useContext } from "react";
+import { useState, useContext } from "react";
 import ItemNotFound from "./Components/Items/ItemNotFound";
-import CartProvider from './ContextAPI/CartProvider.jsx'
+import CartProvider from "./ContextAPI/CartProvider.jsx";
 import Cart from "./Components/Cart/Cart";
 function App() {
   const Itemctx = useContext(ItemsContext);
   console.log(Itemctx.value);
-  const Path = "/"+Itemctx.value;
+  const Path = "/" + Itemctx.value;
   console.log(Path);
 
-  const[isShown,SetisShown]=useState(false);
-  
-  const clicked = ()=>{
+  const [isShown, SetisShown] = useState(false);
+
+  const clicked = () => {
     SetisShown(true);
-  }
-  const closed=()=>{
+  };
+  const closed = () => {
     SetisShown(false);
-  }
-
-
+  };
 
   return (
     <CartProvider>
-      {isShown && <Cart closed={closed}/>}
- <Navbar clicked={clicked} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path={Path} element={<ItemsList />} />
-          <Route path="*" element={<ItemNotFound />} />
-
-        </Routes>
+      {isShown && <Cart closed={closed} />}
+      <Navbar clicked={clicked} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path={Path} element={<ItemsList />} />
+        <Route path="*" element={<ItemNotFound />} />
+      </Routes>
     </CartProvider>
-
-      
-       
-    
   );
 }
 
