0d9995401651864b1da3550876245caf1205f0f6
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-09-08 22:27:16 +0530

    updated

--- MealMagic/src/App.jsx
+++ MealMagic/src/App.jsx
@@ -7,11 +7,18 @@ import { useState, useContext } from "react";
 import ItemNotFound from "./Components/Items/ItemNotFound";
 import CartProvider from "./ContextAPI/CartProvider.jsx";
 import Cart from "./Components/Cart/Cart";
+import {loadStripe} from  "@stripe/stripe-js"
+
+
 function App() {
   const Itemctx = useContext(ItemsContext);
   console.log(Itemctx.value);
   const Path = "/" + Itemctx.value;
   console.log(Path);
+
+  const PaymentKey = "pk_test_51NnoXmSFHehR7P6bVlnz2V2A5VkVFjJYd8Veu7SZNvQYvT35zXs2f9e4dKHu6iyIJkm0uXc1rKjjjq0ksiWBEagA00hLiQAEzM"
+  const StripePromise = loadStripe(PaymentKey);
+  console.log(StripePromise);
 
   const [isShown, SetisShown] = useState(false);
 
